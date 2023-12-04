cf243d6a7c221900a795ed807ff2ba1ad6ca54ae
HyunsooZo <bzhs1992@icloud.com>
2023-12-05 04:47:53 +0900

    Chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/dayofliberation/DayOfLiberationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayofliberation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayOfLiberationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
